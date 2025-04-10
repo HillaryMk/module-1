e5984c7bd41c05c8be6a2cc580d76d829422b2c9
Hillarymk <hillarymk00@gmail.com>
2025-04-10 10:34:06 +0530

    debuged forloop multiplesof3

--- multiplesOf3.js
+++ multiplesOf3.js
@@ -1,7 +1,9 @@
-const readline=require(readline).createInterface{(
-    Input:process.stdin,
-    Output:process.stdout 
-)}
+const readline=require(readline).createInterface(
+    {
+    input:process.stdin,
+    output:process.stdout
+ }
+)
 
 // ask for input from the user
 let userInput =prompt("enter a nmuber:");
@@ -10,7 +12,7 @@ if(isNall(number)|| number <=0) {
     comsole.log("enter a valid positve number" );}
 else {
     console.log("multiple of 3 and 5 up to 100");
-for(let i=1;i<= 100,i++) {
+for(let i=1;i<= 100;i++) {
     if (i % 3==0 || i % 5==0){
         console.log(i);
     }
